483c4ee685d7de59a2ae08516727693d0e57a0fc
YuiCW <yui.chadamon@gmail.com>
2024-02-20 11:52:31 -0600

    msg api created

--- src/test/java/com/example/demo/controller/WelcomeController.java
+++ src/test/java/com/example/demo/controller/WelcomeController.java
@@ -1,5 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+	@GetMapping("/msg")
+	public String getMsg()
+	{
+		return "Hello World";
+			
+	}
 
 }
+
